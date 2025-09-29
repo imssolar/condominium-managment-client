ec487992d29c855e42e4f8a29df9cebfbb764aba
imssolar <i.montecinossolar@gmail.com>
2025-09-29 11:08:28 -0300

    feat: add PackageService with getAllPendingPackages method

--- src/app/services/packages.ts
+++ src/app/services/packages.ts
@@ -1,8 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PackageResponse } from '../models/package';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class Packages {
-  
+export class PackagesService {
+  private readonly API_URL = 'http://localhost:8080/api';
+
+  constructor(private http: HttpClient) {}
+
+  getAllPendingPackages(): Observable<PackageResponse[]> {
+    return this.http.get<PackageResponse[]>(`${this.API_URL}/packages/pending`);
+  }
 }
