b794b83eb8b56eb54850bf764ddaa9ad86162354
imssolar <i.montecinossolar@gmail.com>
2025-10-08 11:40:43 -0300

    feat: create package form

--- src/app/packages/create/create.ts
+++ src/app/packages/create/create.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create.html',
-  styleUrl: './create.css'
+  styleUrl: './create.css',
 })
 export class PackagesCreateComponent {
+  createPackageForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.createPackageForm = this.fb.group({
+      trackingNumber: ['', Validators.required],
+      courier: ['', Validators.required],
+      recipientName: ['', Validators.required],
+      apartmentNumber: ['', Validators.required],
+      description: ['', Validators.min(3)],
+      notes: ['', Validators.max(50)],
+    });
+  }
 }
