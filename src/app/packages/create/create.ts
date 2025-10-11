8426887aa127c66b831b4d7603c9e0dc6a9e1985
imssolar <i.montecinossolar@gmail.com>
2025-10-11 15:06:22 -0300

    feat: create form

--- src/app/packages/create/create.ts
+++ src/app/packages/create/create.ts
@@ -21,4 +21,31 @@ export class PackagesCreateComponent {
       notes: ['', Validators.max(50)],
     });
   }
+
+  errorMessages: { [key: string]: { [key: string]: string } } = {
+    trackingNumber: { required: 'El número de pedido es obligatorio.' },
+    courier: { required: 'La empresa de envío es obligatoria.' },
+    recipientName: { required: 'El nombre del destinatario es obligatorio.' },
+    apartmentNumber: { required: 'El número de apartamento es obligatorio.' },
+    description: { min: 'La descripción debe tener al menos 3 caracteres.' },
+    notes: { max: 'Las notas no pueden exceder los 50 caracteres.' },
+  };
+
+  getErrorMessage(fieldName: string): string {
+    const field = this.createPackageForm.get(fieldName);
+    if (field && field.errors) {
+      const firstErrorKey = Object.keys(field.errors)[0];
+      return this.errorMessages[fieldName][firstErrorKey] || 'Error de validación';
+    }
+    return '';
+  }
+
+  onSubmit() {
+    if (this.createPackageForm.valid) {
+      console.log('Package Created', this.createPackageForm.value);
+      // Aquí puedes agregar la lógica para enviar el formulario al servidor
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
